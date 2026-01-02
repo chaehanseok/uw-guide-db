808ad02e4dd5c01a41f074b0fc23d0342590114c
chaehanseok <47456376+chaehanseok@users.noreply.github.com>
2026-01-02 15:22:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -216,16 +216,18 @@ asof_yyyymmdd = get_db_asof_from_github(DB_URL)
 
 st.title("질병 심사 가이드\n(Underwriting Guide)")
 
-msg = f"""
+st.error(
+    f"""
 이 자료는 미래에셋생명 LoveAge Plan 질병심사메뉴얼을
-({asof_yyyymmdd}) 기준으로 수집한 자료입니다.<br>
-본 자료는 미래에셋금융서비스 구성원 대상 내부 교육자료이며,
-질병별 인수기준은 수시로 변경될 수 있습니다.<br>
-고객 안내 및 청약 전에는 반드시 최신 인수기준을 확인하시기 바랍니다.<br>
-"""
+({asof_yyyymmdd}) 기준으로 수집한 자료입니다.
 
-with st.error(""):
-    st.markdown(msg, unsafe_allow_html=True)
+본 자료는 미래에셋금융서비스 구성원 대상 내부 교육자료이며,
+질병별 인수기준은 수시로 변경될 수 있습니다.
+
+고객 안내 및 청약 전에는 반드시 최신 인수기준을 확인하시기 바랍니다.
+"""
+)
+
 
 # -------------------------
 # DB 로드
@@ -338,6 +340,7 @@ else:
     df_view["decision_show"] = df_view["decision"].replace("", "(빈값)")
     df_view = df_view[df_view["decision_show"].isin(selected)].drop(columns=["decision_show"])
     st.dataframe(df_view, use_container_width=True)
+
 
 
 
